076002b53c126de09d02e5a97706da54b0175de0
this-is-saras <132567959+this-is-saras@users.noreply.github.com>
2024-07-14 13:17:14 +0000

    commit conversion and comparision

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -18,8 +18,22 @@ let boolValue = true //1
 //"abc12" => NaN
 //true=1, false=>0
 
-let isLoggedIn = 1
+let isLoggedIn = 1 //" "-> false "saru" -> true
 
 let boolIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(boolIsLoggedIn);
+console.log(boolIsLoggedIn);//true
+
+
+//***************Operators*******************
+
+let value = 3
+let negValue = -value
+console.log(negValue) //-3
+
+console.log(2+2);
+console.log(2-2);
+console.log("1" + 2);
+console.log("2" + "1");//21
+console.log("1" + 2 +2);//122
+console.log(2+2+"1") //41
